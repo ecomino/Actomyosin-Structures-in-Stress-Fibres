b9ec289fd362ac1d9b62433ddcb5ad229855e11d
ecomino <emma.christine.comino@gmail.com>
2022-02-25 13:05:08 +1000

    Fixed generalised gradient flow

--- Generalised-Gradient-Flow/generalised-gradient-flow.jl
+++ Generalised-Gradient-Flow/generalised-gradient-flow.jl
@@ -9,14 +9,14 @@ U = Array{Float64, 1}(undef, N)
 U[1] = u_1 
 
 for n in 1:N-1
-    u_n = U[n]
-    @show f(u) = (u-u_n)^2/(2*Δt) + Φ(u)
-    U[n+1] = optimize(f, U[n], U[n]+Δt)
+    f(u) = (u-U[n])^2/(2*Δt) + Φ(u)
+    next_u = optimize(f, U[n]-2*Δt, U[n])
+    U[n+1] = Optim.minimizer(next_u)
 end
 
-x = collect(-u0:0.01:u0)
+x = collect(-u_1:0.01:u_1)
 plot(x, Φ.(x), label="Φ(u)=u^2/2", legend=:bottomright, color=:black, lw=0.25)
-scatter!(u, Φ.(u), label="", markersize=3, markerstrokewidth=0, color=:blue)
+scatter!(U, Φ.(U), label="", markersize=3, markerstrokewidth=0, color=:blue)
 title!("Finding a Local Minimum")
 xlabel!("u")
 ylabel!("Φ(u)")

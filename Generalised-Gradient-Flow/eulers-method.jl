ea42eccb51ca854873972a891e35507cc7b9ad5f
Emma Comino <s4587958@uq.edu.au>
2022-02-25 14:17:08 +1000

    Changed search interval

--- Generalised-Gradient-Flow/eulers-method.jl
+++ Generalised-Gradient-Flow/eulers-method.jl
@@ -14,6 +14,7 @@ for n in 1:N-1
     u[n+1] = u[n] / (1 + Δt)
 end
 
+# Plot Φ(u) against u
 x = collect(-u0:0.01:u0)
 plot(x, Φ.(x), label="Φ(u)=u^2/2", legend=:bottomright, color=:black, lw=0.25)
 scatter!(u, Φ.(u), label="", markersize=3, markerstrokewidth=0, color=:blue)
@@ -22,27 +23,9 @@ xlabel!("u")
 ylabel!("Φ(u)")
 savefig("eulers-method-1.png")
 
-#= Another function
-Φ(u) = u^2/2 # function to minimise
-dΦ(u) = -u # its derivative
-u0 = 10 # initial guess
-
-# Euler's method
-Δt = 0.1 # time step
-N = 100 # max number of iterations
-u = Array{Float64, 1}(undef, N)
-u[1] = u0 
-
-# Perform iterations
-for n in 1:N-1
-    u[n+1] = u[n] / (1 + Δt)
-end
-
-x = collect(-u0:0.01:u0)
-plot(x, Φ.(x), label="Φ(u)=u^2/2", legend=:bottomright, color=:black, lw=0.25)
-scatter!(u, Φ.(u), label="", markersize=3, markerstrokewidth=0, color=:blue)
-
+# Plot u against t
+scatter(collect(1:N).*Δt, u, legend=false, markersize=3, markerstrokewidth=0, color=:blue)
 title!("Finding a Local Minimum")
-xlabel!("u")
-ylabel!("Φ(u)")
-savefig("eulers-method-2.png") =#
+xlabel!("t")
+ylabel!("u")
+savefig("eulers-method-2.png")
--- Generalised-Gradient-Flow/generalised-gradient-flow.jl
+++ Generalised-Gradient-Flow/generalised-gradient-flow.jl
@@ -10,7 +10,7 @@ U[1] = u_1
 
 for n in 1:N-1
     f(u) = (u-U[n])^2/(2*Δt) + Φ(u)
-    next_u = optimize(f, U[n]-2*Δt, U[n])
+    next_u = optimize(f, -u_1, u_1) # assume minimum ∈ [-u_1, u_1]
     U[n+1] = Optim.minimizer(next_u)
 end
 
@@ -21,3 +21,9 @@ title!("Finding a Local Minimum")
 xlabel!("u")
 ylabel!("Φ(u)")
 savefig("generalised-gradient-flow-1.png")
+
+scatter(collect(1:N).*Δt, U, legend=false, markersize=3, markerstrokewidth=0, color=:blue)
+title!("Finding a Local Minimum")
+xlabel!("t")
+ylabel!("u")
+savefig("generalised-gradient-flow-2.png")

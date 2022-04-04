ea285d05926deeb215d39d85fd26a4adddb3dfdb
ecomino <emma.christine.comino@gmail.com>
2022-04-04 11:46:40 +1000

    Supervisor meeting 04 Apr 2022

--- Developing-a-Model/model.jl
+++ Developing-a-Model/model.jl
@@ -1,11 +1,13 @@
 using Plots, Optim, LinearAlgebra
-N = 10 # Number of actin filaments
-X = [rand(N)] # Centre point of filaments
-L = 1 # Length of filaments
+const N = 10 # Number of actin filaments
+const L = 1 # Length of filaments
+const Δt = 0.1 # Step size
 anim = Animation()
+
 dist(x1, x2) = abs(x1-x2) 
 overlap(x1,x2) = max(L - dist(x1,x2), 0)
-A = [overlap(x1,x2) for x1 in X[1], x2 in X[1]] # Symmetric Matrix where A[i,j] is the overlap between filament i and j
+
+A(Xvec) = [overlap(x1,x2) for x1 in Xvec, x2 in Xvec] # Symmetric Matrix where A[i,j] is the overlap between filament i and j
 
 function plot_fibres(x)
     X_span = -10:10 # Range for filament movement
@@ -19,18 +21,29 @@ function plot_fibres(x)
     frame(anim)
 end
 
-F = 0.1 #* Matrix(I,3,1) # Pulling factor
-Δt = 0.1 # Step size
-T = 10 # Number of time steps
-for n in 1:T-1
-    E(x) = sum((x-X[end]).^2/2*Δt) - x[1] * F
-    next_x = optimize(E, last(X))
-    push!(X, Optim.minimizer(next_x))
-    plot_fibres(X[end])
+function E(Xtest, Xold, Amat, F)
+    res=sum((Xtest-Xold).^2/2*Δt) - Xtest[1] * F
+    for i in 1:N
+        for j in 1:N
+            res += Amat[i,j]*(Xtest[i]-Xtest[j]-(Xold[i]-Xold[j]))^2/(2*Δt)
+        end
+    end    
+    return res
 end
 
-gif(anim, "test.gif", fps=5)
+function main()
+    X = [rand(N)] # Centre point of filaments
+    F = 0.1 #* Matrix(I,3,1) # Pulling factor
+    T = 10 # Number of time steps
+    for n in 1:T-1
+        
+        next_x = optimize(x->E(x, X, A(last(X)), F), last(X))
+        push!(X, Optim.minimizer(next_x))
+        plot_fibres(X[end])
+    end
 
+    gif(anim, "test.gif", fps=5)
+end
 
 # Assume cross linker proteins cause drag friction -> Overlap between filaments causes drag
 

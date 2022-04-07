b3eec1963b083dca78f80ad161253e6a53b7986a
ecomino <emma.christine.comino@gmail.com>
2022-04-07 12:04:15 +1000

    Added overlap drag

--- Developing-a-Model/model.jl
+++ Developing-a-Model/model.jl
@@ -1,13 +1,14 @@
 using Plots, Optim, LinearAlgebra
+
 const N = 10 # Number of actin filaments
-const L = 1 # Length of filaments
+const L = 0.5 # Length of filaments
 const Δt = 0.1 # Step size
 anim = Animation()
 
-dist(x1, x2) = abs(x1-x2) 
-overlap(x1,x2) = max(L - dist(x1,x2), 0)
-
-A(Xvec) = [overlap(x1,x2) for x1 in Xvec, x2 in Xvec] # Symmetric Matrix where A[i,j] is the overlap between filament i and j
+# Compute overlap between two fibres x1 and x2
+overlap(x1,x2) = max(L - abs(x1-x2), 0)
+# Symmetric Matrix where A[i,j] is half the overlap between filament i and j and diag = 0
+A(x) = [x1 == x2 ? 0 : overlap(x1,x2) for x1 in x, x2 in x] 
 
 function plot_fibres(x)
     X_span = -10:10 # Range for filament movement
@@ -21,29 +22,31 @@ function plot_fibres(x)
     frame(anim)
 end
 
-function E(Xtest, Xold, Amat, F)
-    res=sum((Xtest-Xold).^2/2*Δt) - Xtest[1] * F
+# Energy function where 
+    # x...the next iterate (minimiser)
+    # xn...is the previous
+    # A_mat...is the overlap matrix and 
+    # F...the pulling factor
+function E(x, xn, A_mat, F)
+    res = ξ * sum((x-xn).^2/2*Δt) - x[1] * F
     for i in 1:N
         for j in 1:N
-            res += Amat[i,j]*(Xtest[i]-Xtest[j]-(Xold[i]-Xold[j]))^2/(2*Δt)
+            res += η * A_mat[i,j] * (x[i]-x[j]-(xn[i]-xn[j]))^2/(2*Δt)
         end
     end    
     return res
 end
 
 function main()
-    X = [rand(N)] # Centre point of filaments
-    F = 0.1 #* Matrix(I,3,1) # Pulling factor
+    X = [-4 .* rand(N)] # Centre point of filaments
+    F = 1 # Pulling factor
     T = 10 # Number of time steps
     for n in 1:T-1
-        
-        next_x = optimize(x->E(x, X, A(last(X)), F), last(X))
+        next_x = optimize(x->E(x, X[end], A(X[end]), F), X[end])
         push!(X, Optim.minimizer(next_x))
         plot_fibres(X[end])
     end
 
-    gif(anim, "test.gif", fps=5)
+    gif(anim, "stress-fibres-4.gif", fps=5)
 end
-
-# Assume cross linker proteins cause drag friction -> Overlap between filaments causes drag
 
